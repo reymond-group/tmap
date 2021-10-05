6a7eb9e58d014290c03bfcdad6c22d03f24b3b2d
Daniel Probst <daenuprobst@gmail.com>
2021-10-05 21:34:56 +0200

    Migrating to github actions

--- tmap/recipe/bld.bat
+++ tmap/recipe/bld.bat
@@ -1,4 +1,4 @@
-SET CMAKE_GENERATOR=Visual Studio 14 2015
+SET CMAKE_GENERATOR=Visual Studio 15 2017
 "%PYTHON%" setup.py install --prefix=%PREFIX%
 REM "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
