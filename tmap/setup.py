747cb383e21a7140f1eae0fabc6ee5736133e19a
Daniel Probst <daenuprobst@gmail.com>
2021-10-16 22:23:18 +0200

    Added -DPYBIND11_CPP_STANDARD=/std:c++17

--- tmap/setup.py
+++ tmap/setup.py
@@ -45,6 +45,7 @@ class CMakeBuild(build_ext):
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
             "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DPYBIND11_CPP_STANDARD=/std:c++17"
         ]
 
         cfg = "Debug" if self.debug else "Release"
