77bc5a7778bd8f31f9a524d8a95e6f2251dee25f
Daniel Probst <daenuprobst@gmail.com>
2021-10-31 15:04:30 +0100

    changed to pip install

--- tmap/recipe/bld.bat
+++ tmap/recipe/bld.bat
@@ -1,4 +1,5 @@
-"%PYTHON%" setup.py install --prefix=%PREFIX%
+@REM "%PYTHON%" setup.py install --prefix=%PREFIX%
+%PYTHON% -m pip install . -vv --no-build-isolation
 @REM "%PYTHON%" setup.py install
 
 @REM if errorlevel 1 exit 1
