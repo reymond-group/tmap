a3415d38dd1696735caaacac8b766cf90defa5c3
Daniel Probst <daenuprobst@gmail.com>
2021-10-31 15:16:42 +0100

    Removed prefix option for win

--- tmap/recipe/bld.bat
+++ tmap/recipe/bld.bat
@@ -1,5 +1,4 @@
 @REM "%PYTHON%" setup.py install --prefix=%PREFIX%
-%PYTHON% -m pip install . -vv --no-build-isolation
-@REM "%PYTHON%" setup.py install
+"%PYTHON%" setup.py install
 
 @REM if errorlevel 1 exit 1
