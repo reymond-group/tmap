32b82323e7b0446ad423911eb3179fe8d7cbe99f
Daniel Probst <daenuprobst@gmail.com>
2021-12-26 21:11:17 +0100

    chore(python): added plotting and matplotlib dependency

--- tmap/src/tmap/__init__.py
+++ tmap/src/tmap/__init__.py
@@ -2,5 +2,7 @@ from _tmap import *
 
 from .embedding import embed
 
-# from .plotting import plot
+# Plotting is only available with installed matplotlib
+from .plotting import plot
+
 import tmap.layout_generators as layout_generators
