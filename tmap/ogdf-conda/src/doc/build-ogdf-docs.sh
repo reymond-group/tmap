b7d6d19af9de6ce7d5399cd9a8dc1308fac77798
Daniel Probst <daenuprobst@gmail.com>
2022-05-12 17:55:43 +0200

    chore(build): restructures folders moving ./tmap/* -> ./*

--- tmap/ogdf-conda/src/doc/build-ogdf-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# folder wich contains the script
-folder="$(dirname $0)" || exit 0 # abort on failure
-
-cd "$folder" || exit 0 # abort on failure
-rm -rf ./html
-doxygen "ogdf-doxygen.cfg"
