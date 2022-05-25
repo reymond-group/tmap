dcd90048eb228c741b7ec5089bbc4f9ea1a770f5
Daniel Probst <daenuprobst@gmail.com>
2022-05-25 13:15:25 +0200

    feat(annoy): added annoy dependency to build

--- src/tmap/layout_generators/__init__.py
+++ src/tmap/layout_generators/__init__.py
@@ -1,7 +1,3 @@
 from .base_layout_generator import BaseLayoutGenerator
 from .builtin_layout_generator import BuiltinLayoutGenerator
-
-try:
-    from .annoy_layout_generator import AnnoyLayoutGenerator
-except ImportError:
-    ...
+from .annoy_layout_generator import AnnoyLayoutGenerator
